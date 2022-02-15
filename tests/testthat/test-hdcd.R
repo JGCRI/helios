60ec5d9f0a9e5ffaf95c6c4d0b672471d5033ef0
Zarrar Khan <zarrar.khan@pnnl.gov>
2022-02-15 10:05:53 -0500

    Updating tests

--- tests/testthat/test-hdcd.R
+++ tests/testthat/test-hdcd.R
@@ -1,7 +1,24 @@
 context("hdcd")
 library(helios); library(testthat); library(dplyr)
 
-test_that("hdcd works", {
+#.......................
+# Prepare data for tests
+#.......................
 
+# To do: Get smaller example to test
+# hdcd(ncdf = "example_ncdf_wrfout_d01_1979-01-01_00_00_00.nc",
+#      spatial = "gcamusa",
+#      temporal = NULL,
+#      population = NULL,
+#      reference_temp_F = 65,
+#      #folder = folder_i,
+#      diagnostics= F,
+#      xml= F,
+#      name_append = "",
+#      save = F) -> test_hdcd
+
+# Test outputs hdcd_comb
+test_that("hdcd_comb works", {
+  #testthat::expect_true(nrow(test_hdcd$hdcd_comb)>1)
   testthat::expect_true(1==1)
 })
